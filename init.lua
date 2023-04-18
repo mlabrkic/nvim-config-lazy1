832808292a796efbaca49b89c0a267e4e77593a8
mlabrkic <m1brkic23@gmail.com>
2023-04-18 21:43:22 +0200

    init.lua: hrsh7th/cmp-buffer

--- init.lua
+++ init.lua
@@ -93,7 +93,9 @@ require('lazy').setup({
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-buffer',  -- mlabrkic
+    },
   },
 
   -- Useful plugin to show you pending keybinds.
@@ -487,6 +489,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = "buffer", keyword_length = 2 },  -- mlabrkic
   },
 }
 
