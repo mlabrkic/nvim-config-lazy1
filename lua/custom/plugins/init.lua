08e249dd9e6e79d273985fa6346adac8abc0831b
mlabrkic <m1brkic23@gmail.com>
2023-04-20 19:09:38 +0200

    custom\plugins\init.lua:

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+  -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+  -- { 'j-hui/fidget.nvim', opts = {} },
+
+return {
+
+  { 'windwp/nvim-autopairs', event = "InsertEnter",   -- autopair, Lua
+    config = function()
+      require('nvim-autopairs').setup()
+    end,
+  },
+
+  -- manipulate character pairs quickly, saiw" , sr"( , sd(
+  { 'machakann/vim-sandwich', event = 'VimEnter',
+    config = function()
+      -- require('custom.config.v_vim-sandwich')
+      vim.cmd( [[
+      nmap s <nop>
+      omap s <nop>
+      ]] )
+    end,
+  },
+
+
+
+
+
+
+}
