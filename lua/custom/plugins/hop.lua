7ce015bbed58ec8e8735d6a45bffee86861b041d
mlabrkic <m1brkic23@gmail.com>
2023-04-25 16:22:13 +0200

    custom\plugins: hop.nvim

---
+++ lua/custom/plugins/hop.lua
@@ -0,0 +1,51 @@
+return {
+
+  -- Super fast buffer jump
+  -- Hop is an EasyMotion-like plugin
+  { "phaazon/hop.nvim",
+    cmd = "HopWord",
+    keys = { 'f', 'F', 't', 'T' },
+    branch = 'v2', -- optional but strongly recommended
+    -- enabled = false,
+    opt = {
+      case_insensitive = true,
+      char2_fallback_key = '<CR>',
+    },
+    config = function()
+      require'hop'.setup{}
+
+      vim.cmd[[ hi HopNextKey cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
+      vim.cmd[[ hi HopNextKey1 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
+      vim.cmd[[ hi HopNextKey2 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
+
+      local hop = require('hop')
+      local directions = require('hop.hint').HintDirection
+
+      -- Type a bigram
+--[[
+      vim.keymap.set('', 'f', function()
+        hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      end, {remap=true})
+
+      vim.keymap.set('', 'F', function()
+        hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+      end, {remap=true})
+
+      vim.keymap.set('', 't', function()
+        hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
+      end, {remap=true})
+
+      vim.keymap.set('', 'T', function()
+        hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+      end, {remap=true})
+]]
+
+      vim.keymap.set('', 'f', function() hop.hint_char2() end, {remap=true})
+      vim.keymap.set('', 'F', function() hop.hint_char2() end, {remap=true})
+      vim.keymap.set('', 't', function() hop.hint_char2({ hint_offset = -1 }) end, {remap=true})
+      vim.keymap.set('', 'T', function() hop.hint_char2({ hint_offset = 1 }) end, {remap=true})
+    end,
+  },
+
+
+}
